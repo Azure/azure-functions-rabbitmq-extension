b0cfcd14d1e7474a4400f33169de5a4d24fc910a
katiecai <katiecai@MININT-1MGO0V3.localdomain>
2019-08-27 12:22:47 -0700

    bit of cleanup

--- test/WebJobs.Extensions.RabbitMQ.Tests/BasicDeliverEventArgsToPocoConverterTests.cs
+++ test/WebJobs.Extensions.RabbitMQ.Tests/BasicDeliverEventArgsToPocoConverterTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
 using System.Text;
 using Microsoft.Azure.WebJobs.Extensions.RabbitMQ;
 using Microsoft.Azure.WebJobs.Logging;
--- test/WebJobs.Extensions.RabbitMQ.Tests/RabbitMQListenerTests.cs
+++ test/WebJobs.Extensions.RabbitMQ.Tests/RabbitMQListenerTests.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
 using System.Text;
-using System.Threading;
 using Microsoft.Azure.WebJobs.Extensions.RabbitMQ;
 using Microsoft.Azure.WebJobs.Host.Executors;
 using Microsoft.Extensions.Logging;
