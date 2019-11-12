76b40b6ecf77fd4e699ad03ed5180ea719c593f4
Roger Creyke <roger.creyke@odds-bods.com>
2019-11-12 12:58:53 +0000

    Added ability to set a custom routing key per message.

---
+++ src/RabbitMQMessage.cs
@@ -0,0 +1,14 @@
+﻿namespace Microsoft.Azure.WebJobs.Extensions.RabbitMQ
+{
+    public class RabbitMQMessage
+    {
+        public RabbitMQMessage(byte[] body)
+        {
+            Body = body;
+        }
+
+        public byte[] Body { get; set; }
+
+        public string RoutingKey { get; set; }
+    }
+}
