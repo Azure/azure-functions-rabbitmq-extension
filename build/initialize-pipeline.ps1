a42c2dd461556cf67097cca09a9bf416c7224c76
yojagad <yojagad@microsoft.com>
2020-10-23 19:30:13 -0700

    Testing initial yml

---
+++ build/initialize-pipeline.ps1
@@ -0,0 +1,23 @@
+# Figure out the build number
+$version = [System.IO.File]::ReadAllText("$pwd\\version.txt")
+
+$buildReason = $env:BUILD_REASON
+$branch = $env:BUILD_SOURCEBRANCH
+$buildNumber = $env:BUILD_BUILDNUMBER
+
+if ($buildReason -eq "PullRequest") {
+  # parse PR title to see if we should pack this
+  $response = Invoke-RestMethod api.github.com/repos/$env:BUILD_REPOSITORY_ID/pulls/$env:SYSTEM_PULLREQUEST_PULLREQUESTNUMBER
+  $title = $response.title.ToLowerInvariant()
+  Write-Host "Pull request '$title'"
+  if ($title.Contains("[pack]")) {
+    Write-Host "##vso[task.setvariable variable=BuildArtifacts;isOutput=true]true"
+    Write-Host "Setting 'BuildArtifacts' to true."
+  }
+}
+elseif (-not($branch -contains "rel")) {
+	$version = "$version.$buildNumber"
+}
+
+Write-Host "Version is $version"
+Write-Host "##vso[task.setvariable variable=BuildNumber;isOutput=true]$version"
