782d83f419028e7f800a530de419e66d88b85a41
Yogesh Jagadeesan <yojagad@microsoft.com>
2020-10-27 17:41:10 -0700

    [pack] Added build yml and introduced versioning (#102)
    Added yml and introduced versioning.

---
+++ build/initialize-pipeline.ps1
@@ -0,0 +1,24 @@
+param(
+	[Parameter(Mandatory=$true)][string]$versionPath
+)
+
+# Figure out the build number
+$xml = [Xml] (Get-Content "$versionPath\\WebJobs.Extensions.RabbitMQ.csproj")
+$version = ([string]($xml.Project.PropertyGroup.Version)).Trim()
+
+$buildReason = $env:BUILD_REASON
+$branch = $env:BUILD_SOURCEBRANCH
+
+if ($buildReason -eq "PullRequest") {
+  # parse PR title to see if we should pack this
+  $response = Invoke-RestMethod api.github.com/repos/$env:BUILD_REPOSITORY_ID/pulls/$env:SYSTEM_PULLREQUEST_PULLREQUESTNUMBER
+  $title = $response.title.ToLowerInvariant()
+  Write-Host "Pull request '$title'"
+  if ($title.Contains("[pack]")) {
+    Write-Host "##vso[task.setvariable variable=BuildArtifacts;isOutput=true]true"
+    Write-Host "Setting 'BuildArtifacts' to true."
+  }
+}
+
+Write-Host "Version is $version"
+Write-Host "##vso[task.setvariable variable=BuildNumber;isOutput=true]$version"
