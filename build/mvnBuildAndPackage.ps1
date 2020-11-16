63743d6d66d3ce59e45b106f8fc51c793d8c0097
yojagad <yojagad@microsoft.com>
2020-11-16 15:34:24 -0800

    Testing package

--- build/mvnBuildAndPackage.ps1
+++ build/mvnBuildAndPackage.ps1
@@ -1,14 +1,20 @@
-cd ../binding-library/java
+cd binding-library/java
 
 #Build artifact and version
 [XML]$xmlContent = Get-Content pom.xml
 $artifactId = $xmlContent.project.artifactId
 $version = $xmlContent.project.version
-$outputXmlFileName = "$artifactid-$version.xml"
-Write-Host "##vso[task.setvariable variable=MvnPackagePrefix;isOutput=true]$outputXmlFileName"
+$outputXmlFileName = "$artifactid-$version"
+# Write-Host "##vso[task.setvariable variable=MvnPackagePrefix;isOutput=true]$outputXmlFileName"
 
 #Build
-#.\mvnBuild.bat
+.\mvnBuild.bat
 
 #Copy pom.xml to target fileName
-Copy-Item pom.xml "target\$outputXmlFileName"
+Copy-Item pom.xml "target\$outputXmlFileName.xml"
+
+#Copy to artifact staging directory
+Copy-Item -Path "target\$outputXmlFileName.xml" -Destination $(Build.ArtifactStagingDirectory) -ErrorAction Stop -Verbose -Force
+Copy-Item -Path "target\$outputXmlFileName.jar" -Destination $(Build.ArtifactStagingDirectory) -ErrorAction Stop -Verbose -Force
+Copy-Item -Path "target\$outputXmlFileName-javadoc.jar" -Destination $(Build.ArtifactStagingDirectory) -ErrorAction Stop -Verbose -Force
+Copy-Item -Path "target\$outputXmlFileName-sources.jar" -Destination $(Build.ArtifactStagingDirectory) -ErrorAction Stop -Verbose -Force
