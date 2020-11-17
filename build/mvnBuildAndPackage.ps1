a4ccb66240111b15e9f2c1411d90d6fc1bb2d533
yojagad <yojagad@microsoft.com>
2020-11-16 16:55:25 -0800

    Modified script and yml

--- build/mvnBuildAndPackage.ps1
+++ build/mvnBuildAndPackage.ps1
@@ -1,6 +1,12 @@
+param(
+	[Parameter(Mandatory=$true)][string]$mvnPrefix
+)
+
+Write-Host "Mvn prefix is $mvnPrefix"
+
 #Build
 cd binding-library/java
 .\mvnBuild.bat
 
 #Copy pom.xml to target fileName
-Copy-Item pom.xml "target\$outputXmlFileName.xml"
+Copy-Item pom.xml "target\$mvnPrefix.xml"
