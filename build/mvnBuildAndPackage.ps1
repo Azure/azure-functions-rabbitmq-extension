67085a4cc7b3d8db68249e7c6d66771704f8d16f
yojagad <yojagad@microsoft.com>
2020-11-18 15:15:09 -0800

    Updated directories

--- build/mvnBuildAndPackage.ps1
+++ build/mvnBuildAndPackage.ps1
@@ -1,5 +1,6 @@
 param(
-	[Parameter(Mandatory=$true)][string]$mvnPrefix
+	[Parameter(Mandatory=$true)][string]$mvnPrefix,
+	[Parameter(Mandatory=$true)][string]$mvnVersion
 )
 
 Write-Host "Mvn prefix is $mvnPrefix"
@@ -9,11 +10,11 @@ cd binding-library/java
 .\mvnBuild.bat
 
 #Copy items to be published
-mkdir ToBePublished
-Copy-Item pom.xml "ToBePublished\$mvnPrefix.pom"
-Copy-Item "target\$mvnPrefix.jar" "ToBePublished\$mvnPrefix.jar"
-Copy-Item "target\$mvnPrefix-javadoc.jar" "ToBePublished\$mvnPrefix-javadoc.jar"
-Copy-Item "target\$mvnPrefix-sources.jar" "ToBePublished\$mvnPrefix-sources.jar"
+mkdir ToBePublished\$mvnVersion
+Copy-Item pom.xml "ToBePublished\$mvnVersion\$mvnPrefix.pom"
+Copy-Item "target\$mvnPrefix.jar" "ToBePublished\$mvnVersion\$mvnPrefix.jar"
+Copy-Item "target\$mvnPrefix-javadoc.jar" "ToBePublished\$mvnVersion\$mvnPrefix-javadoc.jar"
+Copy-Item "target\$mvnPrefix-sources.jar" "ToBePublished\$mvnVersion\$mvnPrefix-sources.jar"
 
 
 
