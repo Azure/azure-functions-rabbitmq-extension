a0829293df0e9644b2a595c08213223f36efbe37
yojagad <yojagad@microsoft.com>
2020-12-16 14:13:08 -0800

    Added comments for public properties.

--- src/Trigger/RabbitMQTriggerAttribute.cs
+++ src/Trigger/RabbitMQTriggerAttribute.cs
@@ -23,19 +23,42 @@ namespace Microsoft.Azure.WebJobs
             QueueName = queueName;
         }
 
+        /// <summary>
+        /// Gets or sets the name of app setting that contains the connection string to authenticate with RabbitMQ.
+        /// </summary>
         [ConnectionString]
         public string ConnectionStringSetting { get; set; }
 
+        /// <summary>
+        /// Gets or sets the HostName used to authenticate with RabbitMQ.
+        /// This is ignored if 'ConnectionStringSetting' is set.
+        /// </summary>
         public string HostName { get; set; }
 
+        /// <summary>
+        /// Gets the QueueName to receive messages from.
+        /// </summary>
         public string QueueName { get; }
 
+        /// <summary>
+        /// Gets or sets the name of the app setting containing the username to authenticate with RabbitMQ.
+        /// The app setting name needs to be enclosed between %%. Eg: { UserNameSetting: "%UserNameFromSettings%" }.
+        /// This is ignored if 'ConnectionStringSetting' is set.
+        /// </summary>
         [AppSetting]
         public string UserNameSetting { get; set; }
 
+        /// <summary>
+        /// Gets or sets the name of the app setting containing the password to authenticate with RabbitMQ.
+        /// The app setting name needs to be enclosed between %%. Eg: { PasswordSetting: "%PasswordFromSettings%" }.
+        ///  This is ignored if 'ConnectionStringSetting' is set.
+        /// </summary>
         [AppSetting]
         public string PasswordSetting { get; set; }
 
+        /// <summary>
+        /// Gets or sets the Port used. Defaults to 0.
+        /// </summary>
         public int Port { get; set; }
     }
 }
